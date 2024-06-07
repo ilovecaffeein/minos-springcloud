6a4fc40460ffd454056e0caea53d9e91198191d8
ilovecaffeein <shuaixiaofeng@caxins.com>
2024-06-07 18:19:22 +0800

    init

--- minos-config-service/src/main/java/com/caxs/MinosConfigServiceApplication.java
+++ minos-config-service/src/main/java/com/caxs/MinosConfigServiceApplication.java
@@ -2,10 +2,7 @@ package com.caxs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 /***
  * @ClassName(类名) : MinosConfigServiceApplication
  * @Description(描述) : minos-config-service-配置中心服务端
@@ -13,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @date (开发日期) ： 2020/9/16 14:45
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class MinosConfigServiceApplication {
     public static void main( String[] args ) {
