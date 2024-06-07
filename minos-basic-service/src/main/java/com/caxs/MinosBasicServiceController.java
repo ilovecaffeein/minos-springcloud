8b565d7f652d11659cd2af168446b1e6fc3bccad
ilovecaffeein <shuaixiaofeng@caxins.com>
2024-06-07 18:36:20 +0800

    init

--- minos-basic-service/src/main/java/com/caxs/MinosBasicServiceController.java
+++ minos-basic-service/src/main/java/com/caxs/MinosBasicServiceController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class MinosBasicServiceController {
 
-     @Value("${version}")
+     @Value("${message}")
      String result;
 
      @RequestMapping("/configInfo")
